c412b3c3cefb2ab7c0106b7aac8bc31b38c73ee0
Pavlo Oryniak <pavlo.oryniak@gmail.com>
2021-08-23 22:18:11 +0300

    Initial commit

---
+++ RandomUser/RandomUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomUser
+//
+//  Created by AnyMac on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
